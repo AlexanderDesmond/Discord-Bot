13de625cb54986ec93baa839ae56573fdb42c7e4
Alexander Desmond <AlexanderDesmond848898@gmail.com>
2019-11-21 16:28:18 +1100

    Get bot to turn on and log in.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log(process.env.GITHUB_TOKEN);
+// Get Discord bot token
+require("dotenv/config");
+const TOKEN = process.env.DISCORD_TOKEN;
+// Set up bot
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+// Turn bot on
+client.on("ready", () => {
+  console.log("Bot connected.");
+});
+
+// Sign bot in
+client.login(TOKEN);
