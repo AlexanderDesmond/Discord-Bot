66964aaea484c74b4bd31224ed6362711446309e
Alexander Desmond <AlexanderDesmond848898@gmail.com>
2019-11-22 17:28:50 +1100

    Have the bot return the latency and have it repeat a word or phrase.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 require("dotenv/config");
 const TOKEN = process.env.DISCORD_TOKEN;
 // Set up bot
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client, RichEmbed } = require("discord.js");
+
+// Create new bot.
+const client = new Client();
 
 // Sign bot in
 client.login(TOKEN);
@@ -18,11 +20,56 @@ client.on("ready", () => {
 });
 
 // Have bot respond to messages
-client.on("message", msg => {
-  if (
-    msg.content.toLocaleLowerCase() === "hello" ||
-    msg.content.toLocaleLowerCase() === "hi"
-  ) {
-    msg.channel.send(`Hi ${msg.author}!`);
+client.on("message", async msg => {
+  const prefix = "!";
+
+  if (msg.author.bot) return; // Ignore messages from bots.
+  if (!msg.guild) return; // Ensure messages come from the server.
+  if (!msg.content.startsWith(prefix)) return; // Ensure the message starts with the prefix
+
+  // Remove the prefix and transform the message into an array of strings for each word.
+  const args = msg.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  // Remove the first element of the away and decapitalise it.
+  const cmd = args.shift().toLocaleLowerCase();
+
+  // Have bot give the latency.
+  if (cmd === "ping") {
+    const botMsg = await msg.channel.send("Pinging...");
+    botMsg.edit(
+      `Pong!\n Latency is ${Math.floor(
+        botMsg.createdAt - msg.createdAt
+      )}ms\nAPI Latency ${Math.round(client.ping)}ms`
+    );
+  }
+
+  // Have bot repeat a word or phrase.
+  if (cmd === "say") {
+    // Delete the users's message.
+    if (msg.deletable) msg.delete();
+
+    // If there is no message after the command.
+    if (args.length < 1)
+      return msg.reply("Nothing to say!").then(m => m.delete(5000));
+
+    // Set the colour of an embed to match the bots's colour in the server
+    const roleColour =
+      msg.guild.me.displayHexColor === "#000000"
+        ? "#FFFFFF"
+        : msg.guild.me.displayHexColor;
+
+    // Have the bot repeat a word or phrase (embedding is optional).
+    if (args[0].toLocaleLowerCase() === "embed") {
+      const embed = new RichEmbed()
+        .setColor(roleColour)
+        .setDescription(args.slice(1).join(" "))
+        .setTimestamp()
+        .setAuthor(msg.author.username, msg.author.displayAvatarURL);
+      msg.channel.send(embed);
+    } else {
+      msg.channel.send(args.join(" "));
+    }
   }
 });
