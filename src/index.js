025446e15ef56a6dbf4714bc78d333bf7c6ca93a
Alexander Desmond <AlexanderDesmond848898@gmail.com>
2019-11-22 16:29:57 +1100

    Reformat code.

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ const TOKEN = process.env.DISCORD_TOKEN;
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+// Sign bot in
+client.login(TOKEN);
+
 // Turn bot on
 client.on("ready", () => {
-  console.log("Bot connected.");
+  console.log(`${client.user.username} is online.`);
 
   client.channels
     .find(channel => channel.name === "discord-bot")
     .send("Hello! I'm a bot!");
 });
-
-// Sign bot in
-client.login(TOKEN);
 
 // Have bot respond to messages
 client.on("message", msg => {
